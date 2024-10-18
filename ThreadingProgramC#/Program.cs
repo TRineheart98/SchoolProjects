c627d018d6a13459fd54daf4d7884f656c19a6fd
Tyler Rineheart <157066290+DrPine33@users.noreply.github.com>
2024-10-18 09:25:50 -0500

    Add files via upload

---
+++ ThreadingProgramC#/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int threadCount = 0;
+
+Thread t = new Thread(() => Write("Hello, this is Tyler Rineheart!"));
+t.Start();
+t.Join();  
+
+Thread t2 = new Thread(() => Write("Hello, this is NSU!"));
+t2.Start();
+t2.Join();
+
+Console.WriteLine(); 
+Console.WriteLine("Total number of threads used: " + threadCount);
+
+void Write(string message)
+{
+    try
+    {
+        threadCount++;
+        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: {message}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error in thread {Thread.CurrentThread.ManagedThreadId}: {ex.Message}");
+    }
+}

bcaa53bc3253ce3ea0523249ab8cfc2345db893c
Tyler Rineheart <157066290+DrPine33@users.noreply.github.com>
2024-10-18 09:12:49 -0500

    Add files via upload

---
+++ StringManipulation&ValidationC#/CoolOrNot.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Who is the coolest person in Harringtons advanced programming class?");
+string input = Console.ReadLine();
+
+if(IsTylerOrRineheart(input))
+{
+    Console.WriteLine("Thats right!");
+}
+else
+{
+    Console.WriteLine("No thats not quite correct but they are probably pretty cool aswell!");
+}
+
+static bool IsTylerOrRineheart(string name) =>
+    name.ToUpper() is var upper && (upper == "TYLER" || upper == "RINEHEART");
+

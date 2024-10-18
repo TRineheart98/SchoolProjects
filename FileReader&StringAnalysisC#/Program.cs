cc4b4031dc108ac3f4ade7c07eb1159168b8747f
Tyler Rineheart <157066290+DrPine33@users.noreply.github.com>
2024-10-18 09:22:43 -0500

    Add files via upload

---
+++ FileReader&StringAnalysisC#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class TheFileReaderArraySplit
+{
+    static void Main()
+    {
+        try
+        {
+            string filePath = "textFile.txt";
+            string[] readLines = File.ReadAllLines(filePath);
+            string firstLine = readLines[0];
+
+            var words = Regex.Split(firstLine, @"\W+|\s+");
+
+            int wordsOverFive = words.Count(word => word.Length > 5);
+
+            Console.WriteLine("Amount of words over 5 in length = " + wordsOverFive);
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine($"Error: File not found. Details: {ex.Message}");
+        }
+    }
+}
